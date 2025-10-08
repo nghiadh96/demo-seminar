ab4d0de32c8908825635689cce5a3db77082012b
nghiadhneo <nghiadh@neo.vn>
2025-10-08 14:05:26 +0700

    feat: base

--- src/utils/badUtils.js
+++ src/utils/badUtils.js
@@ -1,6 +1,7 @@
 // File JavaScript với nhiều lỗi ESLint và Prettier
 
 // Lỗi: biến không được sử dụng
+const unusedGlobalVar = "not used anywhere";
 
 // Lỗi: function không được sử dụng
 function unusedFunction() {
@@ -8,18 +9,27 @@ function unusedFunction() {
 }
 
 // Lỗi: biến không được định nghĩa
+console.log(undefinedGlobalVar);
 
 // Lỗi: thiếu dấu chấm phẩy
+const missingSemicolon = "test";
 
 // Lỗi: console.log không được khuyến khích
+console.log("Multiple console statements");
+console.warn("Warning message");
+console.error("Error message");
 
 // Lỗi: object không đúng format
 const badObject = { a: 1, b: 2, c: 3, d: 4 };
 
 // Lỗi: array không đúng format
+const badArray = [1, 2, 3, 4, 5];
 
 // Lỗi: function có nhiều vấn đề
 function badFunction(param1, param2) {
+    const unusedLocalVar = "not used";
+    console.log("Inside function");
+    console.log(undefinedVar);
     return param1 + param2;
 }
 
@@ -34,6 +44,7 @@ var unusedVar = "unused";
 const anotherVar = "test";
 
 // Lỗi: console.log không được khuyến khích
+console.log("Final console log");
 
 // Lỗi: object không đúng format
 const anotherBadObject = { name: "John", age: 30, city: "New York" };
