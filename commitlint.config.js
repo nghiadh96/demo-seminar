92701903c5e071db53415873e29f6ac90e6db150
nghiadhneo <nghiadh@neo.vn>
2025-10-09 16:04:04 +0700

    feat(husky): add message commit

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'],
+    ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 100],
+    'scope-empty': [2, 'never'],
+  },
+}
+// typeof(scope): subject
